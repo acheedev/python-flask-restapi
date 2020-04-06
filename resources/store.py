fb6e656eb015c05fdeea26b4d5ffd4e93bb4042b
acheedev <acheedev@gmail.com>
2020-04-05 20:58:02 -0600

    initial application

---
+++ resources/store.py
@@ -0,0 +1,38 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.store import StoreModel
+
+
+class Store(Resource):
+
+    @jwt_required()
+    def get(self, name: str):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': 'Store not found'}, 404
+
+    @jwt_required()
+    def post(self, name: str):
+        if StoreModel.find_by_name(name):
+            return {"message": f"Store {name} already exists."}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the store"}, 500
+        return store.json(), 201
+
+    @jwt_required()
+    def delete(self, name: str):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+        return {'message': 'Store deleted'}, 200
+
+
+class StoreList(Resource):
+    @jwt_required()
+    def get(self):
+        return {"stores": [store.json() for store in StoreModel.query.all()]}
---
+++ resources/user.py
@@ -0,0 +1,23 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help="This field cannot be null")
+    parser.add_argument('password', type=str, required=True, help="This field cannot be null")
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        user = UserModel.find_by_username(data['username'])
+
+        if user:
+            return {"message": "User already exists"}, 400
+
+        user = UserModel(**data)
+        try:
+            user.save_to_db()
+        except:
+            return {"message": "Failed to create user"}, 500
+        return {"message": "User created successfully."}, 201
